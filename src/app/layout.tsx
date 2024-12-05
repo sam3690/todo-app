71cc31e913dd1d5dfc2c87f26cb7f13c5ac48f8f
sam3690 <usamabinayoub@gmail.com>
2024-12-05 15:42:46 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`flex flex-col bg-[#0D0714] items-center justify-between antialiased`}
       >
         {children}
       </body>
